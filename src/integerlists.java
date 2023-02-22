e24bddd01d7e5fa2f19588f871fcc252da740f23
Your Name <clement.ng15@gmail.com>
2023-02-22 18:51:14 +0800

    Finished integerlist it wasn't even graded, why did i do that

--- src/integerlists.java
+++ src/integerlists.java
@@ -3,37 +3,41 @@ public class integerlists {
         Kattio io = new Kattio(System.in, System.out);
         int N = io.getInt();
         for (int i = 0; i < N; i++) {
-            interpretor(io.getWord(),io.getInt(),io.getWord(),io);
+            interpretor(io.getWord(), io.getInt(), io.getWord(), io);
         }
         io.close();
     }
 
-    static void interpretor(String commands, int numCount,String rawList, Kattio io) {
-        String[] list = rawList.substring(1,rawList.length()-1).split(",");
+    static void interpretor(String commands, int numCount, String rawList, Kattio io) {
+        String[] list = rawList.substring(1, rawList.length() - 1).split(",");
         String[] cmdArray = commands.split("");
         int start = 0;
-        int end = list.length;
+        int end = numCount;
         int reverseCount = 0;
-        for (String dudes: cmdArray) {
+        for (String dudes : cmdArray) {
             if (dudes.equals("R")) {
                 reverseCount++;
-            } else if (reverseCount%2 == 0) {
+            } else if (reverseCount % 2 == 0) {
                 start++;
             } else {
                 end--;
             }
         }
-        if (start > list.length || end < 0 || numCount == 0) {
+        if (start > end) {
             io.println("error");
+        } else if (start == end) {
+            io.println("[]");
+        } else if (rawList.equals("[]")) {
+            io.println(rawList);
         } else {
             io.print("[");
-            if (reverseCount%2 == 0) {
-                for (int i = start; i < end; i++) {
+            if (reverseCount % 2 == 0) {
+                for (int i = start; i < end - 1; i++) {
                     io.print(list[i] + ",");
                 }
-                io.println("]");
+                io.println(list[end - 1] + "]");
             } else {
-                for (int i = end-1; i > start; i--) {
+                for (int i = end - 1; i > start; i--) {
                     io.print(list[i] + ",");
                 }
                 io.println(list[start] + "]");
@@ -42,3 +46,4 @@ public class integerlists {
         }
     }
 }
+
